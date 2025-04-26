e188f075e18c361e1ab05e556c65be70367b31ba
do2523 <do262431@gmail.com>
2025-04-26 02:29:03 -0400

    ENV changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-// next.config.js or next.config.mjs (make sure your file extension matches your syntax)
-
-import "./src/env.js"; // this ensures env validation happens
 /**
- * @type {import('next').NextConfig}
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
  */
-const config = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {};
 
 export default config;
